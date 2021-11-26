8552ff74ef825d5c09f44cd8a6f55ba0b62254be
Code Sandbox <hello@codesandbox.io>
2021-11-26 14:46:03 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.476dc542226a89179b0e042b1590589d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9f9f67a8b22166c37c0430bb2beff29",
+    "url": "https://bruno-morandiere.github.io/csb-l0ery/index.html"
+  },
+  {
+    "revision": "5320dc3e755c788508be",
+    "url": "https://bruno-morandiere.github.io/csb-l0ery/static/css/2.77463b7e.chunk.css"
+  },
+  {
+    "revision": "5320dc3e755c788508be",
+    "url": "https://bruno-morandiere.github.io/csb-l0ery/static/js/2.cc75d34e.chunk.js"
+  },
+  {
+    "revision": "ae930c0bcddeb903fa6f",
+    "url": "https://bruno-morandiere.github.io/csb-l0ery/static/js/main.2af0a30d.chunk.js"
+  },
+  {
+    "revision": "d3d6f9d2a3f4c4aa45aa",
+    "url": "https://bruno-morandiere.github.io/csb-l0ery/static/js/runtime~main.fae9d4f9.js"
+  }
+]);
